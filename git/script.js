4664e8de07154c0291221daef7b961a1523c9f75
RashCod <rashad.aliev.001@gmail.com>
2022-08-18 18:37:48 +0400

    isIsogram

--- git/script.js
+++ git/script.js
@@ -2,3 +2,8 @@ function nameShuffler(str) {
     return str.split(' ').reverse('').join(' ')
 }
 
+
+function isIsogram(str) {
+    let arr = str.toUpperCase().split('')
+    return new Set(arr).size == arr.length;
+}
