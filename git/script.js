cfe2cbfd7ac35196475e215004d38a7972206c40
RashCod <rashad.aliev.001@gmail.com>
2022-08-18 19:29:11 +0400

    findShort

--- git/script.js
+++ git/script.js
@@ -7,3 +7,10 @@ function isIsogram(str) {
     let arr = str.toUpperCase().split('')
     return new Set(arr).size == arr.length;
 }
+
+
+function findShort(s) {
+    let res = s.split(' ').reduce(function (a, b) { return a.length <= b.length ? a : b })
+    return res.length
+
+}
