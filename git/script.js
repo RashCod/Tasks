59e946ce554040e7d5796108d54180dbc689cf80
RashCod <rashad.aliev.001@gmail.com>
2022-09-01 16:16:58 +0400

    arrayPlusArray

--- git/script.js
+++ git/script.js
@@ -287,3 +287,12 @@ function reverseWords(str) {
 }
 
 console.log(reverseWords("hello world!")) //"world! hello")
+
+
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((acc,el) => el + acc,0)
+  }
+
+
+  console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])) //21))
