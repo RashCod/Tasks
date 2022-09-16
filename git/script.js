d62aa99bcc23fa6f7817d954123a6a9d8b493384
RashCod <rashad.aliev.001@gmail.com>
2022-09-17 00:20:24 +0400

    firstNonConsecutive

--- git/script.js
+++ git/script.js
@@ -438,3 +438,16 @@ function vaporcode(string) {
 }
 
 console.log(vaporcode("Lets go to the movies")); //"L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S")
+
+function firstNonConsecutive(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== 1) {
+      return arr[i];
+    }
+  }
+  return null;
+}
+
+console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]));
+
+console.log(firstNonConsecutive([1, 2, 3, 4, 5, 6, 7, 10]));
